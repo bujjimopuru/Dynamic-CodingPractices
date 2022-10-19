ee1b17985b12758ebce9f994bde438d8c9b0a8a1
bujjimopuru <109704736+bujjimopuru@users.noreply.github.com>
2022-10-19 19:00:52 +0530

    Create index.js

---
+++ CP16/3. HTTP Put Method Practice Page/index.js
@@ -0,0 +1,37 @@
+let userInputEl = document.getElementById("userInput");
+let requestBodyEl = document.getElementById("requestBody");
+let sendPutRequestBtnEl = document.getElementById("sendPutRequestBtn");
+let loadingEl = document.getElementById("loading");
+let requestStatusEl = document.getElementById("requestStatus");
+let httpResponseEl = document.getElementById("httpResponse");
+
+function sendPutHTTPRequest() {
+    let userId = userInputEl.value;
+    let requestUrl = "https://gorest.co.in/public-api/users/" + userId;
+    let requestBody = requestBodyEl.value;
+    let options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer 7036755a806c3b8c91d1081c03b7f99a43118bb0ef84e85745c380ee35a98c1a",
+        },
+        body: requestBody
+    };
+    loadingEl.classList.remove("d-none");
+    requestStatusEl.classList.add("d-none");
+    fetch(requestUrl, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonData) {
+            requestStatusEl.classList.remove("d-none");
+            loadingEl.classList.add("d-none");
+
+            let requestStatus = jsonData.code;
+            let httpResponse = JSON.stringify(jsonData);
+            requestStatusEl.textContent = requestStatus;
+            httpResponseEl.textContent = httpResponse;
+        });
+}
+sendPutRequestBtnEl.addEventListener("click", sendPutHTTPRequest);
